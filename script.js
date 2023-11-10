a0769b9448df877080732f08a2786435d3982ad7
Medini Chopra <medini.chopra_ug23@ashoka.edu.in>
2023-11-11 01:10:19 +0530

    edit the cursor

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.getElementById('dataLink').addEventListener('click', function() {
     // Show data grid
-    showGrid('.data-grid');
+    showGrid('.row');
+    // showGrid('.data-grid');
     
     // Hide dance grid
     hideGrid('.dance-grid');
+    // hideGrid('.data-grid');
 });
 
 document.getElementById('danceLink').addEventListener('click', function() {
@@ -11,8 +13,28 @@ document.getElementById('danceLink').addEventListener('click', function() {
     showGrid('.dance-grid');
     
     // Hide data grid
-    hideGrid('.data-grid');
+    hideGrid('.row');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cursor = document.querySelector('.cursor');
+    const cursorTrail = document.querySelector('.cursor-trail');
+
+    document.addEventListener('mousemove', function(e) {
+        const { clientX, clientY } = e;
+
+        cursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+        cursorTrail.style.transform = `translate(${clientX}px, ${clientY}px)`;
+        cursorTrail.style.opacity = '1';
+
+        // Hide the trail after a short delay
+        setTimeout(() => {
+            cursorTrail.style.opacity = '0';
+        }, 200);
+    });
+});
+
+
 
 // Function to show a grid
 function showGrid(selector) {
