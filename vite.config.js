fe3dec3289cb303efb79aed08d03616c70076c18
Medini Chopra <medini.chopra_ug23@ashoka.edu.in>
2024-01-16 09:11:21 +0530

    fix iframe error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/medinichopra.github.io",
+  base: "/",
   build: {
     rollupOptions: {
       input: {
